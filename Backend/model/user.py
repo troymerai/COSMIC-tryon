6395419b7c3333978fcabb674eb7f303d894fc00
heewoneha <jhjmo0719h@naver.com>
2023-07-19 17:20:14 +0900

    refactor: change mysql to mssql (#7)

--- Backend/model/user.py
+++ Backend/model/user.py
@@ -1,6 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import pytz
 import os
 
 from common import db
@@ -9,8 +8,14 @@ load_dotenv()
 
 
 class User(db.Model):
+    __table_args__ = {'schema': os.getenv('SCHEMA_NAME')}
     __tablename__ = os.getenv('USER_TABLE_NAME')
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(255), unique=True, nullable=False)
     user_pw = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.TIMESTAMP(timezone=True), default=datetime.now(pytz.timezone('Asia/Tokyo')), nullable=False)
+    created_at = db.Column(
+        db.TIMESTAMP(timezone=True),
+        default=datetime.utcnow() + timedelta(hours=9),
+        nullable=False
+    )
