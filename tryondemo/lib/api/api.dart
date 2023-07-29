19ce2a894a827c913c6f7af00b11b2d078a19cde
troymerai <16alexandros57@gmail.com>
2023-07-29 21:03:33 +0900

    Feat: 설정 변경 (#8)

--- tryondemo/lib/api/api.dart
+++ tryondemo/lib/api/api.dart
@@ -1,7 +1,7 @@
 class API {
   static const hostConnect = "https://c81a-34-143-232-126.ngrok.io";
   static const hostConnectUser = "$hostConnect/user";
-  static const serverConnect = "http://cosmic.iptime.org:80";
+  static const serverConnect = "";
 
   static const signup = "$serverConnect/tryon/signup";
   static const login = "$serverConnect/tryon/signin";
