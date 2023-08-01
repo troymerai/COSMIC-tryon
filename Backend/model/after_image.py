c581a6bb23a21a993f39171e6ae8f82b6703c0ef
heewoneha <jhjmo0719h@naver.com>
2023-08-01 12:25:04 +0900

    refactor: After Img id col auto increment False (#10)

--- Backend/model/after_image.py
+++ Backend/model/after_image.py
@@ -10,7 +10,7 @@ class AfterImage(db.Model):
     __table_args__ = {'schema': os.getenv('SCHEMA_NAME')}
     __tablename__ = os.getenv('AFTER_TABLE_NAME')
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False, nullable=False)
     user_id = db.Column(db.String(255), nullable=False)
     img_data = db.Column(db.LargeBinary, nullable=False)
     created_at = db.Column(
